ca507745775f28ad18fd5d3ec2bffcc1ed0d87c3
piero <madarp@pobox.com>
2017-11-30 14:55:12 -0500

    Turn down batch size on inceptionv2 model from 24 to 2 to give my feeble local hardware a chance.  Fix logging issues in train.py and setup for training multiple models.

--- train.py
+++ train.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python3
 
 import datetime
+import logging
 import os
 import sys
 import tensorflow as tf
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s [%(threadName)-12s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
+def add_python_paths():
+    # Append the research dir to sys.path. Some components in there must be imported.
+    tf_research_dir = os.getenv('TF_RESEARCH_DIR', '../tensorflow-models/research')
+    if not os.path.exists(tf_research_dir):
+        raise NotADirectoryError('Unable to find %s' % tf_research_dir)
+    if tf_research_dir not in sys.path:
+        sys.path.append(tf_research_dir)
+        sys.path.append(os.path.join(tf_research_dir, 'slim'))
+        logger.info('Using Tensorflow Model Research dir at %s' % tf_research_dir)
+
+
+def setup_logging(level):
+    logger.setLevel(level)
+    tf.logging.set_verbosity(level)
+    # See https://github.com/tensorflow/tensorflow/issues/10498
+    tf.logging._logger.propagate = False
+
 
 def main(_):
     app_start_time = datetime.datetime.now()
-    tf.logging.set_verbosity(tf.logging.DEBUG)
 
-    print(
+    my_level = os.environ.get('LOG_LEVEL', 'INFO')
+    setup_logging(my_level)
+    logger.info(
         '\n'
         '-------------------------------------------------------------------\n'
         '    Running {0}\n'
@@ -19,22 +45,33 @@ def main(_):
         .format(__file__, app_start_time.isoformat())
     )
 
-    # Append the research dir to sys.path. Some components in there must be imported.
-    tf_research_dir = os.getenv('TF_RESEARCH_DIR', '../tensorflow-models/research')
-    if tf_research_dir not in sys.path:
-        sys.path.append(tf_research_dir)
-        sys.path.append(os.path.join(tf_research_dir, 'slim'))
-        print('Using Tensorflow Model Research dir at', tf_research_dir)
+    add_python_paths()
 
-    import object_detection.train as trainer
-    model_dir = os.getenv('TF_MY_MODEL_DIR', 'models')
-    args = ['dummy',
-            '--pipeline_config_path={}/pipeline.config'.format(model_dir),
-            '--train_dir={}/train'.format(model_dir)]
-    tf.app.run(main=trainer.main, argv=args)
+    # Each subdirectory under the 'models' dir contains a downloaded frozen model from
+    # the model zoo.
+    model_list = os.listdir('models')
+    if len(model_list) > 0:
+        import object_detection.train as trainer
+
+        logger.info('Found %d models to train.' % len(model_list))
+        for m in model_list:
+            model_dir = os.path.join('models', m)
+            logger.info('Training the model at %s' % model_dir)
+
+            # Invoke the trainer directly instead of using tf.app.run, because I don't want their early
+            # sys.exit.  So yes-- calling a protected method.
+            trainer.FLAGS._parse_flags()
+            trainer.FLAGS.train_dir = '{}/train'.format(model_dir)
+            trainer.FLAGS.pipeline_config_path = '{}/pipeline.config'.format(model_dir)
+            trainer.main(__file__)
+
+    else:
+        logger.error('No models found.')
+
+    logger.info('Completed training for all discovered models.')
 
     uptime = datetime.datetime.now() - app_start_time
-    print(
+    logger.info(
         '\n'
         '-------------------------------------------------------------------\n'
         '   Completed {0}\n'
