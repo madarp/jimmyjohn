d8e828a5a376bd617ddf2c9ef44bf6a46e52b46a
piero <madarp@pobox.com>
2017-11-30 16:27:31 -0500

    Fix export.py to iterate over all models and export only highest checkpoint found.

--- export.py
+++ export.py
@@ -1,16 +1,51 @@
 #!/usr/bin/env python3
-
+"""
+Iterates over all models in the 'models' directory and exports the inference graphs
+of the highest checkpoint found in each model's 'train' directory.
+"""
 import datetime
+import logging
 import os
 import sys
 import tensorflow as tf
 
+logger = logging.getLogger(__name__)
+
+
+def add_python_paths():
+    # Append the research dir to sys.path. Some components in there must be imported.
+    tf_research_dir = os.getenv('TF_RESEARCH_DIR', '../tensorflow-models/research')
+    if not os.path.exists(tf_research_dir):
+        raise NotADirectoryError('Unable to find %s' % tf_research_dir)
+    if tf_research_dir not in sys.path:
+        sys.path.append(tf_research_dir)
+        sys.path.append(os.path.join(tf_research_dir, 'slim'))
+        logger.info('Using Tensorflow Model Research dir at %s' % tf_research_dir)
+
+
+def setup_logging(level):
+    logger.setLevel(level)
+    tf.logging.set_verbosity(level)
+    # See https://github.com/tensorflow/tensorflow/issues/10498
+    tf.logging._logger.propagate = False
+
+
+def get_max_checkpoint(ckpt_dir):
+    file_list = os.listdir(ckpt_dir)
+    max_ckpt = 0
+    for f in file_list:
+        if 'index' in f:
+            # example filename is 'model.ckpt-9813.data-00000-of-00001'
+            ckpt = int(f.split('.')[1].split('-')[1])
+            max_ckpt = max(ckpt, max_ckpt)
+    return max_ckpt
+
 
 def main(_):
     app_start_time = datetime.datetime.now()
-    tf.logging.set_verbosity(tf.logging.DEBUG)
-
-    print(
+    my_level = os.environ.get('LOG_LEVEL', 'INFO')
+    setup_logging(my_level)
+    logger.info(
         '\n'
         '-------------------------------------------------------------------\n'
         '    Running {0}\n'
@@ -19,34 +54,39 @@ def main(_):
         .format(__file__, app_start_time.isoformat())
     )
 
-    # Append the research dir to PYTHONPATH. Some components in there must be imported.
-    tf_research_dir = os.getenv('TF_RESEARCH_DIR', '../tensorflow-models/research')
-    if not os.path.exists(tf_research_dir):
-        raise NotADirectoryError('Unable to find ', tf_research_dir)
-    if tf_research_dir not in sys.path:
-        sys.path.append(tf_research_dir)
-        sys.path.append(os.path.join(tf_research_dir, 'slim'))
-        print('Using Tensorflow Model Research dir at ', tf_research_dir)
+    add_python_paths()
 
-    import object_detection.export_inference_graph as exp
+    import object_detection.export_inference_graph as eig
 
-    model_dir = os.getenv('TF_MY_MODEL_DIR', 'models')
-    ckpt_num = os.getenv('TF_EXPORT_CKPT_NUM')
-    print('Exporting graph from checkpoint ', ckpt_num)
-    train_dir = os.path.join(model_dir, 'train')
-    out_dir = os.path.join(train_dir, 'export_graph-{}'.format(ckpt_num))
-    ckpt_prefix = os.path.join(train_dir, 'model.ckpt-{}'.format(ckpt_num))
+    model_list = os.listdir('models')
+    if len(model_list) > 0:
+        for m in model_list:
+            model_dir = os.path.join('models', m)
+            train_dir = os.path.join(model_dir, 'train')
+            # find the highest checkpoint
+            ckpt_num = get_max_checkpoint(train_dir)
+            if ckpt_num > 0:
+                out_dir = os.path.join(train_dir, 'export-{}'.format(ckpt_num))
+                ckpt_prefix = os.path.join(train_dir, 'model.ckpt-{}'.format(ckpt_num))
+                logger.info('Exporting checkpoint {} to {}'.format(ckpt_num, out_dir))
 
-    args = ['dummy',
-            '--input_type=image_tensor',
-            '--pipeline_config_path={}/pipeline.config'.format(train_dir),
-            '--trained_checkpoint_prefix={}'.format(ckpt_prefix),
-            '--output_directory={}'.format(out_dir)
-            ]
-    tf.app.run(main=exp.main, argv=args)
+                # Invoke the exporter directly instead of using tf.app.run, because I don't want their early
+                # sys.exit.  So yes-- calling a protected method.
+                eig.FLAGS._parse_flags()
+                eig.FLAGS.input_type = 'image_tensor'
+                eig.FLAGS.pipeline_config_path = '{}/pipeline.config'.format(train_dir)
+                eig.FLAGS.trained_checkpoint_prefix = ckpt_prefix
+                eig.FLAGS.output_directory = out_dir
+                eig.main(__file__)
+            else:
+                logger.warning('No checkpoints found in ', train_dir)
+    else:
+        logger.error('No models found.')
+
+    logger.info('Completed graph checkpoint export for all discovered models.')
 
     uptime = datetime.datetime.now() - app_start_time
-    print(
+    logger.info(
         '\n'
         '-------------------------------------------------------------------\n'
         '   Completed {0}\n'
